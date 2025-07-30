defa2ee51be477ce982c9a9b236d10cf06a25754
Ibrahim Čaušević <92829361+IbrahimCausevic@users.noreply.github.com>
2025-07-30 02:38:34 +0200

    promenaaa

--- script.js
+++ script.js
@@ -38,3 +38,24 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+
+    if (endValue === 100) {
+      valueDisplay.textContent = startValue + "%";
+    } else {
+      valueDisplay.textContent = startValue;
+    }
+
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
